4140891dfb66299c96a6ec9885cb8afb554e245d
LeeHey <lcxfs1991@gmail.com>
2014-11-07 17:05:24 +0800

    clean codes

--- src/js/iarcmenu.js
+++ src/js/iarcmenu.js
@@ -31,7 +31,7 @@ function iArcMenu(opt) {
 	this.diffDist = opt.diffDist || 100;
 
 	//diameter of cicles
-	this.diameter = 50;
+	this.diameter = opt.diameter || 50;
 
 	//another form of degree
 	this.radiusPI = 2 * Math.PI / 360;
@@ -39,10 +39,6 @@ function iArcMenu(opt) {
 	this.duration = opt.duration || 1000;
 
 	this.type = opt.type || 'arc';
-
-	// starting poinst position
-	this.centerX = 0;
-	this.centerY = 0;
 
 	// circle elements
 	this.ele = [];
@@ -55,80 +51,69 @@ function iArcMenu(opt) {
 
 	//calculate menu transform degree and distance
 	this.transformDist();
-
-	//render control button
-	this.renderControlBtn();
-
-	//menu start translate
-	// this.animate();
 }
 
 // render basic menu html
 iArcMenu.prototype.renderHTML = function(){
 
 	var item = false;
-	var subItem = false;
+	// var subItem = false;
 	var len = this.data.length;
 
-	for (var i = 0; i < len; i++) {
-
+	//render control btn at last index
+	for (var i = 0; i <= len; i++) {
 		item = document.createElement("div");
-		item.className = 'arcMenu-shape';
-		item.setAttribute('data-id', i);
-		item.style.backgroundColor = this.data[i].color;
-		item.style.zIndex = (len - i);
+		item.style.width = this.diameter + 'px';
+		item.style.height = this.diameter + 'px';
+		item.style.WebkitBorderRadius = (this.diameter / 10) + 'em';
+		item.style.MozBorderRadius = (this.diameter / 10) + 'em';
+		item.style.BorderRadius = (this.diameter / 10) + 'em';
 
-		subItem = document.createElement("div");
-		subItem.innerHTML = this.data[i].content;
-		item.appendChild(subItem);
+		if (i === len) {
+			item.className = 'iArcMenu-control iArcMenu-control-on';
+			item.style.zIndex = this.data.length;
 
-		this.dom.appendChild(item);
+			this.dom.appendChild(item);
+			this.bindControlTouchHandler(item);
+		}
+		else {
+			item.className = 'iArcMenu-shape';
+			item.setAttribute('data-id', i);
+			item.style.zIndex = (len - i);
+			item.innerHTML = this.data[i].content;
 
-		this.ele.push(item);
-
-		this.bindTouchHandler(i);
+			this.dom.appendChild(item);
+			this.ele.push(item);
+			this.bindItemTouchHandler(i);
+		}
 
 	}
-
-	// if (len > 0) {
-	// 	var center = this.ele[0].getBoundingClientRect();
-	// 	this.centerX = center.left + this.diameter / 2;
-	// 	this.centerY = center.top + this.diameter / 2;
-	// }
 		
 };
 
-iArcMenu.prototype.bindTouchHandler = function(index) {
+iArcMenu.prototype.bindItemTouchHandler = function(index) {
 
 	if (this.data[index].callback) {
 		this.ele[index].addEventListener('touchstart', this.data[index].callback, false);
 	}
 }
 
-iArcMenu.prototype.renderControlBtn = function () {
-
-	item = document.createElement("div");
-	item.className = 'arcMenu-control arcMenu-control-on';
-	item.style.zIndex = this.data.length;
-	subItem = document.createElement("div");
-
-	item.appendChild(subItem);
-	this.dom.appendChild(item);
+iArcMenu.prototype.bindControlTouchHandler = function(control) {
 
 	var self = this;
 	this.controlToggle = 0;
 
-	item.addEventListener('touchstart', function() {
+	control.addEventListener('touchstart', function() {
 		//menu start translate
 		if (self.controlToggle === 0) {
-			self.animate();
+			self.animate('forward');
 			self.controlToggle = 1;
-			this.className = 'arcMenu-control arcMenu-control-off';
+			this.className = 'iArcMenu-control iArcMenu-control-off';
 		}
 		else {
-			self.resume();
+			self.animate('backward');
 			self.controlToggle = 0;
-			this.className = 'arcMenu-control arcMenu-control-on';
+			this.className = 'iArcMenu-control iArcMenu-control-on';
 		}
 		
 	}, false);
@@ -173,14 +158,13 @@ iArcMenu.prototype.transformDist = function() {
 			};
 
 			this.transformData.push(item);
-
 			rotateDegree += this.degree;
 		}
 	}
 }
 
 //menu start translate
-iArcMenu.prototype.animate = function() {
+iArcMenu.prototype.animate = function(type) {
 
 	var len = this.data.length;
 
@@ -189,58 +173,20 @@ iArcMenu.prototype.animate = function() {
 	}
 
 	for (var i = 0; i < len; i++) {
-		this._moveAni(i);
+		this._moveAni(i, type);
 	}
 
 };
 
-//menu translate animation
-iArcMenu.prototype._moveAni = function(index) {
+//menu translate animation, forward or backward animation
+iArcMenu.prototype._moveAni = function(index, type) {
 
 	var ele = this.ele;
 	var transformData = this.transformData;
 
 	setTimeout(function(){
-			ele[index].style.WebkitTransform = 'translate3d('+ transformData[index].x +'px, ' + transformData[index].y + 'px, 0)';
+			ele[index].style.WebkitTransform = (type === 'forward')? 
+				'translate3d('+ transformData[index].x +'px, ' + transformData[index].y + 'px, 0)' :
+				'translate3d(0, 0, 0)';
 	}, this.duration);
-}
-
-//menu resume shopes
-iArcMenu.prototype.resume = function() {
-
-	var len = this.data.length;
-
-	for (var i = 0; i < len; i++) {
-		this.ele[i].style.WebkitTransition = '-webkit-transform 500ms';
-	}
-
-	for (var i = 0; i < len; i++) {
-		this._resumeAni(i);
-	}
-
-};
-
-//menu translate animation
-iArcMenu.prototype._resumeAni = function(index) {
-
-	var ele = this.ele;
-
-	setTimeout(function(){
-			ele[index].style.WebkitTransform = 'translate3d(0, 0, 0)';
-	}, this.duration);
-}
-
-//get position of menu circles after animations
-iArcMenu.prototype._getPos = function() {
-
-	var self = this;
-	var position = false;
-		
-	for (var i = 0; i < self.ele.length; i++) {
-		self.elePos[i] = {};
-		position = self.ele[i].getBoundingClientRect();
-		self.elePos[i].x = position.left + self.diameter / 2;
-		self.elePos[i].y = position.top + self.diameter / 2;
-	}
-
 }
